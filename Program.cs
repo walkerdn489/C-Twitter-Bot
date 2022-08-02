20524308d061d30c6ef05972289c95a5f9d6dcd8
walkerdn489 <walkerdn2020@gmail.com>
2022-08-02 10:29:22 -0700

    updates

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Tweetinvi;
 using Tweetinvi.Models;
 
+using System;
 
 class Program
 {
@@ -10,10 +11,30 @@ class Program
             args[0], args[1], args[2], args[3]
         );
         var user = await userClient.Users.GetAuthenticatedUserAsync();
-        Console.WriteLine(user);
-        var tweet = await userClient.Tweets.PublishTweetAsync("Pears");
-        Console.WriteLine("You published the tweet : " + tweet);
+
+        var timelineTweets = new List<ITweet>();
+        //var timelineIterator = userClient.Timelines.GetHomeTimelineIterator();
+        var timelineIterator = await userClient.Timelines.GetHomeTimelineAsync();
+
+        int total_tweets = 10;
+        int i = 0;
+        while (!timelineIterator.Completed && i != total_tweets)
+        {
+            var page = await timelineIterator.NextPageAsync(); 
+            timelineTweets.AddRange(page);  
+            i++;
+        }   
+
+        Console.WriteLine(timelineTweets[0]);
+       //var tweetinviUser = await userClient.Users.GetUserAsync("POTUS");
+       //var userTimelineTweets = await userClient.Timelines.GetUserTimelineAsync("POTUS");
+
+        
+        //var tweet = await userClient.Tweets.PublishTweetAsync("Pears");
+        //Console.WriteLine("You published the tweet : " + tweet);
 
     }
 }
+
+
 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # newStackAttack
 I wanted to try making a stack with C# and .Net for practice
+This turned into a twitter bot. Will add more info when scope of bot is figured out. 
 by David Walker
